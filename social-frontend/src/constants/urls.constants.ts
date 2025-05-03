41687b3a8c3647494a460c43d2cdaada9f424e86
MaxSkr0210 <71515588+MaxSkr0210@users.noreply.github.com>
2025-05-03 23:20:16 +0300

    update docker-compose file

--- social-frontend/src/constants/urls.constants.ts
+++ social-frontend/src/constants/urls.constants.ts
@@ -1,7 +1,7 @@
 export enum EUrl {
     HOME = '/',
     LOGIN = '/login',
-    REGISTER = '/register',
+    REGISTER = '/registration',
 }
 
 export const NO_AUTH_URL = [EUrl.LOGIN, EUrl.REGISTER];
