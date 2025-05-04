1c107aff27de82f10df0308a6c7f1d80fc7091d3
MaxSkr0210 <71515588+MaxSkr0210@users.noreply.github.com>
2025-05-04 17:05:08 +0300

    update cors

--- back/gateway/src/main.ts
+++ back/gateway/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   app.use(cookieParser());
   app.setGlobalPrefix('/api');
   app.enableCors({
-    origin: ['http://localhost:3001'],
+    origin: ['http://localhost:3001', 'http://mskrinnik.ru'],
     credentials: true,
   });
 
